f5e638c331e0c971794a4bb69e3df01461423654
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-01 23:48:14 +0600

    chefCategories data loaded is running succefully

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const chefCategories=require('./Data/chef.json')
+
+
 app.get('/', (req, res) => {
   res.send('Hello chef master!')
 })
+
+app.get('/chefCategories',(req,res)=>{
+    res.send(chefCategories)
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
