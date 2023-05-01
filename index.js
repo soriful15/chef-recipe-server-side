b83511f3f0370dd0f4aa16e780fe94fded63167d
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-01 23:53:32 +0600

    cors install and access the require the core

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+const cors = require('cors')
 
-const chefCategories=require('./Data/chef.json')
-
+const chefCategories = require('./Data/chef.json')
+app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello chef master!')
+    res.send('Hello chef master!')
 })
 
-app.get('/chefCategories',(req,res)=>{
+app.get('/chefCategories', (req, res) => {
     res.send(chefCategories)
 })
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
