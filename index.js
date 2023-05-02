f817c038b6d0557cff67038827b258b78a2a7052
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-03 02:05:48 +0600

    chefCategories id find

--- index.js
+++ index.js
@@ -14,8 +14,17 @@ app.get('/chefCategories', (req, res) => {
     res.send(chefCategories)
 })
 
-/* https://chef-recipe-hunter-server-tawny.vercel.app/chefCategories */
-// https://www.thebangladeshikitchen.com/
+app.get('/chefCategories/:id',(req,res)=>{
+    const id=req.params.id
+    console.log(id)
+    const selectedChefCategories=chefCategories.find(categories=>categories.id == id)
+    console.log(selectedChefCategories)
+    res.send(selectedChefCategories)
+})
+
+
+
+/* https://chef-recipe-hunter-server-soriful15.vercel.app/chefCategories */
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
